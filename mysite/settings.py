6e07652732030c16f257fd4f1554fb92f5b7aa19
Allen Sollars <allensollars03@gmail.com>
2026-04-28 15:55:12 -0400

    trying again

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,6 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-fiel
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
